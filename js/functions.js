984583f93fdeac05b4a8e693178a15834b1c043d
Jose Luis <jolutase104@gmail.com>
2023-11-25 21:00:04 +0100

    Casi acabado calendario

--- js/functions.js
+++ js/functions.js
@@ -26,6 +26,7 @@ function colocarMes(date){
     let posicionDia1 = dia1DelMes.getDay();
     let numeroDeDiasMes = getNumeroDiaPorMes(date.getMonth()+1,date.getFullYear());
     let numerosDiaMesPasado= getNumeroDiaPorMes(date.getMonth(),(date.getMonth()+1==1)?date.getFullYear()-1:date.getFullYear());
+    localStorage.removeItem('elemento-seleccionado');
     let resta = (posicionDia1==0)?5:posicionDia1-2;
     textoMesYAnyo.textContent = mesesNombre[date.getMonth()]+" "+date.getFullYear();
     numeros.forEach(numero => {
@@ -51,27 +52,93 @@ function colocarMes(date){
                 cambio=false;
                 restaOSuma = 2;
             }
-        numero.id = date.getFullYear()+'-'+(date.getMonth()+restaOSuma)+'-'+((restaOSuma==0)?numerosDiaMesPasado-(resta+1):((dia/10<1)?'0'+(dia-1):dia-1));
-        ponerEventListener(numero,date.getFullYear()+'-'+(date.getMonth()+restaOSuma)+'-'+((restaOSuma==0)?numerosDiaMesPasado-(resta+1):((dia/10<1)?'0'+(dia-1):dia-1)));
-            
+        numero.id = date.getFullYear()+'-'+(((date.getMonth()<10)?'0':'')+(date.getMonth()+restaOSuma))+'-'+((restaOSuma==0)?numerosDiaMesPasado-(resta+1):((dia/10<1)?'0'+(dia-1):dia-1));
     });
-    localStorage.removeItem('elemento-seleccionado');
 }
 
-function ponerEventListener(elemento,fecha){
+ function colocarEventsListeners(numeros){
+    numeros.forEach(element => {
+        ponerEventListener(element);
+    });
+ }
+
+function ponerEventListener(elemento){    
     elemento.addEventListener('click',()=>{
-        if(localStorage.getItem('elemento-seleccionado')!=null){
+        let idElemento= elemento.id;
+        if(parseInt(idElemento.substr(5,2))>date.getMonth()+1){
+            date.setMonth(date.getMonth()+1);
+            colocarMes(date);
+        }else if(parseInt(idElemento.substr(5,2))<date.getMonth()+1){
+            date.setMonth(date.getMonth()-1);
+            colocarMes(date);
+        }else if(localStorage.getItem('elemento-seleccionado')!=null){
             let idElementoAnterior = JSON.parse(localStorage.getItem('elemento-seleccionado'));
-            if(idElementoAnterior.substr(5,2)==fecha.substr(5,2))
+            if(idElementoAnterior.substr(5,2)==date.getMonth()+1)
                 document.getElementById(idElementoAnterior).classList='';
-            else
-                document.getElementById(idElementoAnterior).classList='otro-mes';
         }
-        elemento.classList ="seleccionado";
-        localStorage.setItem('elemento-seleccionado',JSON.stringify(fecha));
+        document.getElementById(idElemento).classList ="seleccionado";
+        localStorage.setItem('elemento-seleccionado',JSON.stringify(idElemento));
+        establecerTareas(idElemento);
+
     });
 }
+function establecerTareas(idElemento){
+    bloqueTareas.innerHTML=`<div class="col-12 row">
+    <h1 class="col-6 ms-3 mb-0">Tareas</h1>
+    <strong class="col-5 ms-3 text-end azul fechaTareas">${parseInt(idElemento.substr(8))+ ' de '+mesesNombre[parseInt(idElemento.substr(5,2))-1]+ ' de '+ date.getFullYear()}</strong>
+</div>
+<div class="contenedor-tareas mt-4" id='contenedorTareas'>
+    
+</div>
+<div class="col-12 d-flex justify-content-around mt-3">
+    <input type="text" id="tareaAnyadir" placeholder="Escribe tarea" class="col-7 rounded">
+    <button class="btn btn-primary col-4" id='anyadir-tarea'>Crear tarea</button>
+</div>`;
+    if(localStorage.getItem('tareas')==null){
+        localStorage.setItem('tareas',JSON.stringify([]));
+    }
+    let allTareas = JSON.parse(localStorage.getItem('tareas'));
+    let tareas = JSON.parse(localStorage.getItem('tareas')).filter(tarea => tarea.id== idElemento);
+    if(tareas.length>0){
+        document.getElementById('contenedorTareas').innerHTML='';
+        tareas.forEach(element => {
+            anyadirElemnto(element.idTarea,element.nombreTarea,document.getElementById('contenedorTareas'),allTareas);
+        });
 
+    }else{
+        document.getElementById('contenedorTareas').innerHTML='<h3 class="col-12 text-center">No hay tareas este día </h3>';
+    }
+    document.getElementById('anyadir-tarea').addEventListener('click',()=>{
+        let inputTarea = document.getElementById('tareaAnyadir');
+        if(inputTarea.value==''){
+            alert('Porfavor para añadir una tarea escriba lo que quiere anyadir');
+        }else{
+            if(tareas.length<1){
+                document.getElementById('contenedorTareas').innerHTML='';
+                anyadirElemnto(allTareas.length-1,inputTarea.value,document.getElementById('contenedorTareas'),allTareas);
+            }else{
+                anyadirElemnto(allTareas.length-1,inputTarea.value,document.getElementById('contenedorTareas'),allTareas);
+            }
+            allTareas.push({idTarea: allTareas.length,nombreTarea: inputTarea.value, nombreUsuario:'manolo'});
+            tareas.push({idTarea: allTareas.length,nombreTarea: inputTarea.value, nombreUsuario:'manolo'});
+            localStorage.setItem('tareas',JSON.stringify(allTareas));
+        }
+    })
+
+}
+
+function anyadirElemnto(idTarea,nombreTarea,padre,allTareas){
+    padre.innerHTML+=`<div class="tarea d-flex justify-content-around align-items-center rounded mb-3">
+    <h3>${nombreTarea}</h3>
+    <i class="bi bi-trash3 rounded" id='${idTarea}'></i>
+    </div>`;
+    document.getElementById(idTarea).addEventListener('click',()=>{
+        allTareas = allTareas.filter(tarea => tarea.idTarea!=idTarea);
+        document.getElementById(idTarea).parentElement.remove();
+        localStorage.setItem('tareas',JSON.stringify(allTareas));
+    });
+
+}
 
 function isBisiesto(año){
     if(año%4==0){
--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const sumarmes = document.getElementById('mesmas');
 const restarmes = document.getElementById('mesmenos'); 
 const textoMesYAnyo = document.getElementById('mesyanyo');
 const mesesNombre = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+const bloqueTareas= document.getElementById('bloque-tarea');
 let date = new Date();
 colocarMes(date);
 sumarmes.addEventListener('click',()=>{
@@ -13,3 +14,4 @@ restarmes.addEventListener('click',()=>{
     date.setMonth(date.getMonth()-1);
     colocarMes(date);
 });
+colocarEventsListeners(numeros);
